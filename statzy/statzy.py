8121a3874b22d98a53bfb822b56f13203f776ae2
LeonKohli <98176333+LeonKohli@users.noreply.github.com>
2023-03-03 00:57:01 +0100

    not fully implemented table view

--- statzy/statzy.py
+++ statzy/statzy.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import psycopg2
 
 statzy = Flask(__name__)
 
@@ -30,5 +30,57 @@ def komponenteServer():
     return render_template('komponenteServer.html')
 
 
+@statzy.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    try:
+        conn = psycopg2.connect(
+            dbname='statzy',
+            user=username,
+            password=password,
+            host='localhost',
+            port='5432'
+        )
+        cursor = conn.cursor()
+
+        # Get the list of tables in the database
+        cursor.execute(
+            "SELECT table_name FROM information_schema.tables WHERE table_schema='public' ORDER BY table_name")
+        tables = [table[0] for table in cursor.fetchall()]
+
+        # Render the template with the dropdown menu
+        return render_template('tables.html', tables=tables)
+    except:
+        return 'Database connection failed!'
+
+
+@statzy.route('/query', methods=['POST'])
+def query():
+    table_name = request.form['table']
+
+    try:
+        conn = psycopg2.connect(
+            dbname='statzy',
+            user=username,
+            password=password,
+            host='localhost',
+            port='5432'
+        )
+        cursor = conn.cursor()
+
+        # Execute the SELECT * query on the selected table
+        cursor.execute(
+            "SELECT * FROM {table_name}")
+        results = cursor.fetchall()
+
+        # Render the template with the query results
+
+        return render_template('query.html', table_name=table_name, results=results)
+    except:
+        return 'Database connection failed!'
+
+
 if __name__ == '__main__':
-    statzy.run()
+    statzy.run(debug=True)
