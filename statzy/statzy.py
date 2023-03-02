849b57864dce97c992ff225fdcb7243d03ef15f5
LeonKohli <kohli.leon@gmail.com>
2023-03-02 15:58:54 +0100

    LOGIN

--- statzy/statzy.py
+++ statzy/statzy.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, Response
-import psycopg2
+from flask import Flask, render_template
 
 
 statzy = Flask(__name__)
@@ -24,43 +23,6 @@ def datenbanken():
 @statzy.route('/server')
 def server():
     return render_template('server.html')
-
-
-@statzy.route('/stammdaten')
-def stammdaten():
-    return render_template('stammdaten.html')
-
-
-@statzy.route('/komponenteServer')
-def komponenteServer():
-    return render_template('komponenteServer.html')
-
-
-@statzy.route('/login', methods=['POST'])
-def login():
-    username = request.form['username']
-    password = request.form['password']
-
-    try:
-        conn = psycopg2.connect(
-            dbname='stratzy',
-            user=username,
-            password=password,
-            host='localhost',
-            port='5432'
-        )
-        # database connection is successful
-        cursor = conn.cursor()
-
-        # execute SQL queries and handle database operations here
-        # ...
-
-        cursor.close()
-        conn.close()
-        return 'Database connection successful!'
-    except:
-        # database connection failed
-        return 'Database connection failed!'
 
 
 if __name__ == '__main__':
