ccece756f01e6b0e6c8f1c549556a51e6d2c633b
LeonKohli <kohli.leon@gmail.com>
2023-03-03 15:23:35 +0100

    LOGIN WORKS

--- statzy/statzy.py
+++ statzy/statzy.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 import psycopg2
+import secrets
 
 statzy = Flask(__name__)
+conn = None
+cursor = None
+statzy.secret_key = secrets.token_hex(16)
 
 
 @statzy.route('/')
@@ -10,14 +14,14 @@ def index():
     return render_template('login.html', title=title)
 
 
-@statzy.route('/fachverfahren', methods=['POST'])
+@statzy.route('/start')
+def start():
+    return render_template('index.html')
+
+
+@statzy.route('/fachverfahren')
 def fachverfahren():
     return render_template('fachverfahren.html')
-
-
-@statzy.route('/datenbanken')
-def datenbanken():
-    return render_template('datenbanken.html')
 
 
 @statzy.route('/server')
@@ -32,15 +36,71 @@ def komponenteServer():
 
 @statzy.route('/login', methods=['POST'])
 def login():
-    global conn
+    global conn, cursor
     username = request.form['username']
     password = request.form['password']
+
+    print("Username:", username)
+    print("Password:", password)
+
     try:
         conn = psycopg2.connect(
             dbname='statzy',
             user=username,
             password=password,
-            host='10.128.201.127',
+            host='localhost',
+            port='5432'
+        )
+
+        cursor = conn.cursor()
+
+        # Get the list of tables in the database
+        cursor.execute(
+            "SELECT table_name FROM information_schema.tables WHERE table_schema='public' ORDER BY table_name")
+        tables = [table[0] for table in cursor.fetchall()]
+
+        # Store the username and password in the session
+        session['username'] = username
+        session['password'] = password
+        print("Session:", session['username'])
+        print("Session:", session['password'])
+        # Redirect to the datenbanken page with the dropdown menu
+        return redirect(url_for('start'))
+
+    except Exception as e:
+        print("Exception:", e)
+        return 'Database connection failed! Login'
+
+
+@statzy.route('/query', methods=['POST'])
+def query():
+    global conn, cursor
+    table_name = request.form['table']
+    try:
+        # Execute the SELECT * query on the selected table
+        cursor.execute(
+            f"SELECT * FROM {table_name}")
+        results = cursor.fetchall()
+
+        # Render the template with the query results
+        return render_template('query.html', table_name=table_name, results=results)
+    except:
+        return 'Database connection failed! Query'
+
+
+@statzy.route('/datenbanken')
+def datenbanken():
+    global conn, cursor
+    try:
+        # Get the username and password from the session
+        username = session.get('username', None)
+        password = session.get('password', None)
+
+        conn = psycopg2.connect(
+            dbname='statzy',
+            user=username,
+            password=password,
+            host='localhost',
             port='5432'
         )
         cursor = conn.cursor()
@@ -50,28 +110,11 @@ def login():
             "SELECT table_name FROM information_schema.tables WHERE table_schema='public' ORDER BY table_name")
         tables = [table[0] for table in cursor.fetchall()]
 
-        # Render the template with the dropdown menu
-        return render_template('index.html', tables=tables)
-    except:
-        return 'Database connection failed!'
+        return render_template('datenbanken.html', tables=tables)
 
-
-@statzy.route('/query', methods=['POST'])
-def query():
-    table_name = request.form['table']
-    try:
-        cursor = conn.cursor()
-
-        # Execute the SELECT * query on the selected table
-        cursor.execute(
-            "SELECT * FROM {table_name}")
-        results = cursor.fetchall()
-
-        # Render the template with the query results
-
-        return render_template('query.html', table_name=table_name, results=results)
-    except:
-        return 'Database connection failed!'
+    except Exception as e:
+        print("Exception:", e)
+        return 'Database connection failed! Datenbanken'
 
 
 if __name__ == '__main__':
