db655ad23379229312e6b8c56bb82616ed7eba2e
LeonKohli <kohli.leon@gmail.com>
2023-05-15 14:59:27 +0200

    Revert "fachverfahren undlogo" s This reverts commit 1fa5de33d440e096657729284757670e06a801da.

--- statzy/statzy.py
+++ statzy/statzy.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, session, g
+from flask import Flask, render_template, request, redirect, url_for, session, g
 import psycopg2
 import secrets
 
