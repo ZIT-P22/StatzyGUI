442dad76ae42d0c10b1f9359469c593603756578
jeremys.social <71183843+jeremyssocial@users.noreply.github.com>
2023-05-15 09:29:07 +0200

    Update statzy.py
    updated database IP

--- statzy/statzy.py
+++ statzy/statzy.py
@@ -161,7 +161,7 @@ def datenbanken():
             dbname='statzy',
             user=username,
             password=password,
-            host='localhost',
+            host='10.128.201.123',
             port='5432'
         )
         cursor = conn.cursor()
