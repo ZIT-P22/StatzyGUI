e40c795ca77ceab0459df88417b95cc65014d27c
Willi <webwilli004@gmail.com>
2023-05-16 11:58:33 +0200

    routen für server

--- statzy/statzy.py
+++ statzy/statzy.py
@@ -378,6 +378,82 @@ def serverAnsehen():
             return render_template('serverAnsehen.html', name=name, verf_id=verf_id, tag=tag, vewendungszweck=vewendungszweck, laufzeitverfahren=laufzeitverfahren, auftraggeber=auftraggeber, verf_betreuung=verf_betreuung, kundenmanagement=kundenmanagement, fachadministration=fachadministration)
         except:
             return 'Fehler'
+   
+        
+@statzy.route('/serverErstellen', methods=['POST'])
+def serverErstellen():
+    name = request.form['name']
+    edit = request.form['edit']
+    if edit == '1':
+        name = request.form['name']
+        verf_id = request.form['verf_id']
+        vewendungszweck = request.form['vewendungszweck']
+        laufzeitverfahren = request.form['laufzeitverfahren']
+        auftraggeber = request.form['auftraggeber']
+        verf_betreuung = request.form['verf_betreuung']
+        kundenmanagement = request.form['kundenmanagement']
+        fachadministration = request.form['fachadministration']
+        # ? wenn auftraggeber, verf_betreuung, kundenmanagement, fachadministration in der person Datenbank vorhanden sind, dann wird das form in die Datenbank fachverfahren geschrieben
+
+        if personValidate(auftraggeber) and personValidate(verf_betreuung) and personValidate(kundenmanagement) and personValidate(fachadministration):
+            try:
+                cursor = get_cursor()
+                print('test 1')
+                query = "INSERT INTO fachverfahren (name, verf_id, vewendungszweck, laufzeitverfahren, auftraggeber, verf_betreuung, kundenmanagement, fachadministration) VALUES ('" + name + "', '" + \
+                    verf_id + "', '" + name + "', '" + vewendungszweck + "', '" + laufzeitverfahren + "', '" + \
+                        auftraggeber + "', '" + verf_betreuung + "', '" + \
+                    kundenmanagement + "', '" + fachadministration + "')"
+                print("test 2")
+                cursor.execute(query)
+                print("test 3")
+                cursor.connection.commit()
+                print("test 4")
+                cursor.close()
+                # debug print(query rückgabe)
+                print('Server wurde erstellt')
+                return render_template('serverAnsehen.html', name=name, verf_id=verf_id, vewendungszweck=vewendungszweck, laufzeitverfahren=laufzeitverfahren, auftraggeber=auftraggeber, verf_betreuung=verf_betreuung, kundenmanagement=kundenmanagement, fachadministration=fachadministration)
+            except Exception as e:
+                return 'Fehler: ' + str(e)
+        else:
+            return 'Diesen Server gibt es nicht '
+
+
+@statzy.route('/serverEditieren', methods=['POST'])
+def serverEditieren():
+    name = request.form['name']
+    try:
+        query = "SELECT name, verf_id, vewendungszweck, laufzeitverfahren, auftraggeber, verf_betreuung, kundenmanagement, fachadministration FROM fachverfahren WHERE tag ~* '" + name + "' ORDER BY name "
+        results = db_execute(query)
+
+        name, verf_id, vewendungszweck, laufzeitverfahren, auftraggeber, verf_betreuung, kundenmanagement, fachadministration = results[
+            0]
+        return render_template('serverEditieren.html', name=name, verf_id=verf_id, vewendungszweck=vewendungszweck, laufzeitverfahren=laufzeitverfahren, auftraggeber=auftraggeber, verf_betreuung=verf_betreuung, kundenmanagement=kundenmanagement, fachadministration=fachadministration)
+    except:
+        return 'Fehler'
+    
+    
+@statzy.route('/serverUpdate', methods=['POST'])
+def serverUpdate():
+    name = request.form['it-verfahren-namen']
+    verf_id = request.form['verfahrens-id']
+    vewendungszweck = request.form['verwendungszweck']
+    laufzeitverfahren = request.form['laufzeit']
+    auftraggeber = request.form['auftraggeber']
+    verf_betreuung = request.form['verf_bet']
+    kundenmanagement = request.form['kundenmanagement']
+    fachadministration = request.form['fachadministration']
+
+    try:
+        cursor = get_cursor()
+        query = """UPDATE server SET name=%s, verf_id=%s, vewendungszweck=%s, laufzeitverfahren=%s, auftraggeber=%s, 
+                verf_betreuung=%s, kundenmanagement=%s, fachadministration=%s WHERE name=%s"""
+        cursor.execute(query, (name, verf_id, vewendungszweck, laufzeitverfahren,
+                               auftraggeber, verf_betreuung, kundenmanagement, fachadministration, name))
+        get_db().commit()
+        return redirect(url_for('fachverfahrenAnsehen', name=name))
+    except Exception as e:
+        return 'Fehler: ' + str(e)
+
 
 @statzy.route('/komponenteServer')
 def komponenteServer():
